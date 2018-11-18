f724c469c0a73d05bd6a486b129d59603c83f3f7
wuly <596707410@qq.com>
2018-11-18 21:53:33 +0800

    wly的Test方法

---
+++ maven_controller/src/main/java/com/example/controller/Test.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+/**
+ * @ClassName Test
+ * @Descrintion TODO
+ * @Author wly
+ * @Date 2018/11/18 21:52
+ * @Version 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
